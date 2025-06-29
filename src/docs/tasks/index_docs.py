028ac56ecfec7555c5f402b6120b40eaa867413d
pinkiepie <pinkiepies0912@gmail.com>
2025-06-30 01:30:56 +0900

    feature: Add retry tasks

--- src/docs/tasks/index_docs.py
+++ src/docs/tasks/index_docs.py
@@ -1,4 +1,5 @@
-from celery import shared_task
+from typing import Dict, List, cast
+from celery import Task, shared_task
 from dependency_injector.wiring import Provide, inject
 from pydantic import BaseModel
 
@@ -22,3 +23,28 @@ def index_docs(
     params = IndexDocsParams(key=request.key)
 
     doc_writer.index_docs(params)
+
+
+@shared_task(name="docs.tasks.retry_indexing_docs")
+@inject
+def retry_indexing_docs(
+    req: List[Dict[str, str]],
+    doc_writer: DocWriter = Provide[CeleryContainer.doc_writer],
+) -> None:
+
+    for row in req:
+        try:
+            request = _IndexDocsRequest(**row)
+            doc_writer.index_docs(IndexDocsParams(key=request.key))
+        except Exception:
+            continue
+
+
+@shared_task(name="docs.tasks.handle_fail_indexing")
+@inject
+def handle_fail_indexing(
+    doc_writer: DocWriter = Provide[CeleryContainer.doc_writer],
+) -> None:
+    doc_writer.retry_unhandled_docs(
+        lambda req: cast(Task, retry_indexing_docs).delay(req)
+    )
