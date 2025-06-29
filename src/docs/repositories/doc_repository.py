76013a260c9a44f2e6dfd79b9687f34d3f219c9b
pinkiepie <pinkiepies0912@gmail.com>
2025-06-30 01:29:23 +0900

    fix: Type

--- src/docs/repositories/doc_repository.py
+++ src/docs/repositories/doc_repository.py
@@ -1,13 +1,8 @@
-from typing import TypeAlias
-
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session
 from docs.models.doc_model import Docs
-
-SessionType: TypeAlias = Session | AsyncSession
 
 
 class DocRepository:
 
-    def create_doc(self, session: SessionType, doc: Docs):
+    def create_doc(self, session: AsyncSession, doc: Docs):
         session.add(doc)
